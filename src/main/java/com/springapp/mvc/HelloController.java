70d4550bb1015f5102afff2496826bf8f6c89d1a
Looker <slooker@amazon.com>
2014-09-04 08:30:07 -0700

    Modified with new awesome spring stuff to handle a POST and JSON.

--- src/main/java/com/springapp/mvc/HelloController.java
+++ src/main/java/com/springapp/mvc/HelloController.java
@@ -2,15 +2,38 @@ package com.springapp.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.Calendar;
+import java.util.HashMap;
 
 @Controller
 @RequestMapping("/")
 public class HelloController {
-	@RequestMapping(method = RequestMethod.GET)
-	public String printWelcome(ModelMap model) {
-		model.addAttribute("message", "Hello world!");
-		return "hello";
-	}
+    // Since there's no value in the RequestMapping for this, it will only match the RequestMapping of "/" declared above the class declaration
+    // This will also only match on a GET request.
+    @RequestMapping(method = RequestMethod.GET)
+    public String printWelcome(ModelMap model) {
+        model.addAttribute("message", "Hello world!");
+        return "hello";
+    }
+
+
+    // This only matches if we go to / from the RequestMapping above the class declaration AND
+    // if its a POST request
+    @RequestMapping(value ="test", method = RequestMethod.POST)
+    // Return whatever format we've configured in the spring XML (in this case mvc-dispatcher-servlet.xml)
+    @ResponseBody
+    public HashMap handlePost(@RequestBody HashMap map) {
+        // Check if there's a password
+        if (map.containsKey("password")) {
+            // Remove password
+            map.remove("password");
+        }
+        // Add date
+        map.put("date", Calendar.getInstance().getTime().toString());
+        return map;
+    }
 }
